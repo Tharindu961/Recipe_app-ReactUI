f99288c15834540307bffa3c3ef4d0c4516763f7
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-07-22 23:52:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   const getRecips = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    console.log(data.hits);
+    setRecipes(data.hits);
   };
 
   return(
