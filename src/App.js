45b9bbd35ba1c98c6d62926cc0087a7cfcaf46e4
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-07-24 12:47:38 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -29,7 +29,11 @@ const App = () => {
   
       </form>
       {recipes.map(recipe => (
-        <Recipe title={recipe.recipe} calories={recipe.recipe.calories}/>
+        <Recipe 
+        title={recipe.recipe}
+        calories={recipe.recipe.calories}
+        image= {recipe.recipe.image}
+        />
       ))}
     </div>
   )
